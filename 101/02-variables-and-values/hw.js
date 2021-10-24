8048fdb805f63126de782afbd35c7394f0ee1ac0
mmusoyan <55488372+mmusoyan@users.noreply.github.com>
2021-10-24 21:10:03 +0400

    Create hw.js

---
+++ 101/02-variables-and-values/hw.js
@@ -0,0 +1,76 @@
+// David
+const stall = {
+  bread: "5",
+  sugar: "name",
+  gum: "2",
+  seller: {
+    firstname: "Mike",
+    age: "20"
+  }
+}
+
+var bed = true;
+var window = true;
+let cupboard = ["clothes", "books", "cars", "knives"];
+const myRoom = [bed, cupboard, window];
+
+
+// Gor
+var room = {
+  walls: {
+    q: 4,
+    color: {
+      shade: '~peach',
+      hex: '~FFB266'
+    },
+    props: {
+      door: true,
+      windows: true,
+      additionalDecor: 1
+    }
+  },
+  furniture: [{
+    bed: {
+      size: 'queen size',
+      condition: 'very old',
+      associations: 'hate'
+    }
+  }, 'table', 'tv stand', 'closet', '~stand', 'chair', 'chair']
+}
+
+var store = {
+    staff: ['owner', 'manager', {
+      cashiers: {
+        q: 12
+      }
+    }, 'cleaners', 'storage workers'],
+    sections: {
+      totalnumber: 10,
+      list: [{
+          food: ['bread', 'meat', 'milk and eggs', 'vegetables', 'drinks', 'snacks and confetionary', 'canned food']
+        }, {
+          hardware: ['i am', 'bored', 'sorry']]
+      }
+             
+             
+             
+// Khach
+var car = {
+  tire: 4,
+  color: "black",
+  glass: "white",
+  rudder: {
+    color: "brown",
+    spinning: true
+  }
+}
+
+var telephone = {
+    buttons: 2,
+    color: "blue",
+    camera: 4,
+    case: {
+     color: "black",
+     material: "rubber"
+    }
+}
