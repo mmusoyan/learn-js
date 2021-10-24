3b13af3e781450147e75fb9e8a870463d116d244
mmusoyan <55488372+mmusoyan@users.noreply.github.com>
2021-10-24 22:17:46 +0400

    Create cw.js

---
+++ 101/02-variables-and-values/cw.js
@@ -0,0 +1,60 @@
+var color = 'red';
+var flower = {
+  species: 'rose',
+  color
+}
+
+// {propertyName: propertyValue}
+
+// Vardan
+
+let bouquetVardan = {
+    vase:'triangle',
+    roses:{
+        color:'creme',
+        quantity: 12
+    },
+    starFlowers: true,
+    decorativeLeaves:true,
+    ribbon:{
+        color:'white',
+        material:'mesh'
+    }
+}
+
+// David
+const buket = {
+  some_decor: true,
+  rose: 14,
+  other_flowers: 20
+}
+ 
+// Khach
+var bouquetKhach = {
+    flowers: 12,
+    color: ["white", "yellow"],
+    design: {
+     vase: {
+        material: "glass",
+        beautiful: false
+     },
+      somethingLikeStars: true,
+    },
+    leaves: {
+     color: "green",
+     amount: "many"
+    }
+}
+
+// Gor
+var bouquetGor = {
+  type: 'rose',
+  color: 'white',
+  quantityOfVisibleBuds: 11,
+  associations: {
+    beautiful: true,
+    green: true,
+    memories: false
+  },
+  conditionDescription: "Maybe fresh, in a vase."
+}
