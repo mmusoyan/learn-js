faccf50227941f90488129d74071670dfaa55d86
mmusoyan <55488372+mmusoyan@users.noreply.github.com>
2021-12-03 22:57:45 +0400

    Update array-methods.js

--- 101/06/array-methods.js
+++ 101/06/array-methods.js
@@ -1,1 +1,16 @@
+forEach
+[1, 2, 5, 7, 9, 85, 68, 968, 100]
+[{name: "Vardan", age: 21}, {name: "Gor", age: 45}, {name: "David", age: 17}, {name: "Vova", age: null}, {name: null, age: 15}]
+
+Array.length
+
+[].length -> 0
+[{name: "Vardan", age: 21}, {name: "Gor", age: 45}, {name: "David", age: 17}, {name: "Vova", age: null}, {name: null, age: 15}].length -> 5
+[1, 2, 5, 7, 9, 85, 68, 968, 100].length -> 9
+
+var students = [{name: "Vardan", age: 21}, {name: "Gor", age: 45}, {name: "David", age: 17}, {name: "Vova", age: null}, {name: null, age: 15}];
+
+students.forEach();
+
+
 
